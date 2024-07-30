60f0b5e8f8d9731613e6513e4bf5756978fdcf99
John Wilkinson <me@jcwilk.com>
2024-07-30 12:53:45 -0700

    Adding vite in with vanilla js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+export default {
+  root: 'src',
+  build: {
+    outDir: '../dist', // Output directory for build
+  },
+  base: '/vite-github-pages-template/', // Replace with your repository name
+};
